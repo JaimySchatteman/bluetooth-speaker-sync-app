20bc7723f92357eb5e5713635ac37448f19f3c27
jaimy <schattemanjaimy@outlook.com>
2021-03-05 13:22:25 +0100

    🎉Start project

---
+++ my-app/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  printWidth: 140,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  useTabs: false,
+  arrowParens: "avoid",
+  trailingComma: "all",
+};
---
+++ my-app/src/App.tsx
@@ -0,0 +1,57 @@
+import React, { FunctionComponent } from "react";
+import "./App.less";
+import { Breadcrumb, Layout, Menu } from "antd";
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from "@ant-design/icons";
+
+const { Sider, Header, Content } = Layout;
+const { SubMenu } = Menu;
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider theme={"dark"} width={200} className="site-layout-background">
+            <Menu mode="inline" defaultSelectedKeys={["1"]} defaultOpenKeys={["sub1"]} style={{ height: "100%", borderRight: 0 }}>
+              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="9" icon={<NotificationOutlined />}>
+                option9
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: "0 24px 24px" }}>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: "100vh",
+              }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+
+export default App;
