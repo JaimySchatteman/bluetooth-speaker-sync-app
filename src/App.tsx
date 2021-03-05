77e6cd645e3abff95214cdcef01417b05e8530e4
jaimy <schattemanjaimy@outlook.com>
2021-03-05 16:28:39 +0100

    First test
    get modal to find devices, display devicename on console

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import "./App.less";
 import { Breadcrumb, Layout, Menu } from "antd";
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from "@ant-design/icons";
+import Test from "./Test";
 
 const { Sider, Header, Content } = Layout;
 const { SubMenu } = Menu;
@@ -45,7 +46,7 @@ const App: FunctionComponent = () => {
                 minHeight: "100vh",
               }}
             >
-              Content
+              <Test />
             </Content>
           </Layout>
         </Layout>
