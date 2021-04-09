da6d5e1410b29a553489953c3791fa6a10af2131
Arne Naessens <arne@tiar.be>
2021-04-09 15:50:25 +0200

    Fix User api

--- bluetooth-speaker-laravel/app/Http/Controllers/UserController.php
+++ bluetooth-speaker-laravel/app/Http/Controllers/UserController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+
+    protected static $rules = [
+        'name' => 'required'
+    ];
+
     public function index()
     {
         return User::all();
@@ -19,13 +26,16 @@ class UserController extends Controller
 
     public function create(Request $request)
     {
-        return User::firstOrCreate($request);
+        return User::firstOrCreate(['name' => $request->name, 'email' => $request->email, 'password' => Hash::make($request->password)]);
     }
 
     public function update(Request $request, $id)
     {
         $user = User::findOrFail($id);
-        $user->update($request);
+        if ($request->password != null) {
+            $request->password = Hash::make($request->password);
+        }
+        $user->update($request->all());
         $user->save();
 
         return $user;
