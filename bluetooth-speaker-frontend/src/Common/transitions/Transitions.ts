063afc65b7d131731f5c59533142a52fa74f92ab
JaimyS <schattemanjaimy@outlook.com>
2021-05-17 10:45:58 +0200

    💫 Adding page transition definitions

---
+++ bluetooth-speaker-frontend/src/Common/transitions/Transitions.ts
@@ -0,0 +1,122 @@
+// @ts-ignore
+import { room, drop, glideIn } from "react-tiger-transition";
+
+room({
+  name: "room-left",
+  direction: "left",
+  angle: 90,
+  enter: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 1,
+    delay: 0,
+  },
+  exit: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 2,
+    delay: 0,
+  },
+});
+
+room({
+  name: "room-right",
+  direction: "right",
+  angle: 90,
+  enter: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 1,
+    delay: 0,
+  },
+  exit: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 2,
+    delay: 0,
+  },
+});
+
+drop({
+  name: "drop-bottom",
+  direction: "bottom",
+  enter: {
+    duration: 800,
+    easing: "ease",
+    opacity: 1,
+    zIndex: 2,
+    delay: 0,
+  },
+  exit: {
+    duration: 800,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 1,
+    scale: 0.6,
+    delay: 0,
+  },
+});
+
+drop({
+  name: "drop-top",
+  direction: "top",
+  enter: {
+    duration: 800,
+    easing: "ease",
+    opacity: 1,
+    zIndex: 2,
+    delay: 0,
+  },
+  exit: {
+    duration: 800,
+    easing: "ease",
+    opacity: 0.3,
+    zIndex: 1,
+    scale: 0.6,
+    delay: 0,
+  },
+});
+
+glideIn({
+  name: "glide-left",
+  direction: "left",
+  enter: {
+    duration: 600,
+    easing: "ease",
+    opacity: 1,
+    zIndex: 4,
+    delay: 0,
+  },
+  exit: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0,
+    zIndex: 2,
+    scale: 1,
+    delay: 0,
+  },
+});
+
+glideIn({
+  name: "glide-right",
+  direction: "right",
+  enter: {
+    duration: 600,
+    easing: "ease",
+    opacity: 1,
+    zIndex: 3,
+    delay: 0,
+  },
+  exit: {
+    duration: 600,
+    easing: "ease",
+    opacity: 0,
+    zIndex: 1,
+    scale: 1,
+    delay: 0,
+  },
+});
