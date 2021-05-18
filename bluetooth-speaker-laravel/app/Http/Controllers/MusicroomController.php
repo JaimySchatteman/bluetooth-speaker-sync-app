d881001290426710d8b848012c587b168e47e644
JaimyS <schattemanjaimy@outlook.com>
2021-05-18 10:27:26 +0200

    🐛 Fixing 500 error

--- bluetooth-speaker-laravel/app/Http/Controllers/MusicroomController.php
+++ bluetooth-speaker-laravel/app/Http/Controllers/MusicroomController.php
@@ -15,13 +15,13 @@ class MusicroomController extends Controller
     //https://stackoverflow.com/questions/19852927/get-specific-columns-using-with-function-in-laravel-eloquent
     public function show($id)
     {
-        return Musicroom::with('queue,queue.tracks')->findOrFail($id);
+        return Musicroom::with('queue.tracks')->findOrFail($id);
     }
 
     public function create(Request $request)
     {
         $musicroom =  Musicroom::firstOrCreate(['title' => $request->title]);
-        $queue = Queue::create(['musicroom_id' => $request->id]);
+        $queue = Queue::create(['musicroom_id' => $musicroom->id]);
 
         return $musicroom;
     }
@@ -38,7 +38,7 @@ class MusicroomController extends Controller
     //https://appdividend.com/2018/05/17/laravel-many-to-many-relationship-example/
     public function destroyTrack($musicroom_id, $track_id){
         $queue = Queue::where('musicroom_id', $musicroom_id);
-        
+
         $track = Track::findOrFail($track_id);
 
         $queue->tracks()->detach($track);
